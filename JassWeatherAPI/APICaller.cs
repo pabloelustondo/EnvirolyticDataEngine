97ac4eeb1ed81c1e0d36d32fe605030eb2375487
Pablo Elustondo <pablo@jassbrain.com>
2013-12-28 18:06:22 -0500

    first version

---
+++ JassWeatherAPI/APICaller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JassWeatherAPI
+{
+    public class APICaller
+    {
+        public string callAPI(string url){
+
+            WebRequest req = WebRequest.Create(url);
+            req.Method = "GET";
+            HttpWebResponse resp = req.GetResponse() as HttpWebResponse;
+            if (resp.StatusCode == HttpStatusCode.OK)
+            {
+                using (Stream respStream = resp.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(respStream, Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            else
+            {
+                return string.Format("Status Code: {0}, Status Description: {1}", resp.StatusCode, resp.StatusDescription);
+            }
+
+        }
+    }
+}
---
+++ JassWeatherAPI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JassWeatherAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get method
+
+            APICaller apiCaller = new APICaller();
+            string request = "http://api.wunderground.com/api/501a82781dc79a42/geolookup/conditions/q/IA/Cedar_Rapids.json";
+            string response = apiCaller.callAPI(request);
+            Console.WriteLine(response);
+            Console.Read();
+
+
+            request = "http://api.wunderground.com/api/501a82781dc79a42/history_20060405/q/CA/San_Francisco.json";
+            response = apiCaller.callAPI(request);
+            Console.WriteLine(response);
+            Console.Read();
+
+
+        }
+    }
+}
