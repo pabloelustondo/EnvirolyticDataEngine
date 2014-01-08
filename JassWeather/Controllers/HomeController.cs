8aa094a69ab79eba96c00a80c310fb0f37a68b7e
Pablo Elustondo <pablo@jassbrain.com>
2014-01-07 19:06:08 -0500

    Finally it works on Azure as well !

--- JassWeather/Controllers/HomeController.cs
+++ JassWeather/Controllers/HomeController.cs
@@ -102,8 +102,96 @@ commandResponse = cmd2.StandardOutput.ReadToEnd() + commandResponse1;
             }
         catch (Exception e)
         {
-           ViewBag.Message = e.Message + e.InnerException.ToString();
+            ViewBag.Message = e.Message; ;
         } 
+
+            ViewBag.Message = "Test executed correctly:" + commandResponse;
+            return View();
+        }
+
+        public ActionResult TestSDS()
+        {
+
+            string commandResponse = "n/a";
+            string commandResponse1 = "n/a";
+
+            try
+            {
+
+                DateTime startTime = DateTime.UtcNow;
+                WebRequest request = WebRequest.Create("http://www.narccap.ucar.edu/data/example/tas_WRFG_example.nc");
+                WebResponse response = request.GetResponse();
+
+
+                string command = string.Format("/c del tas_WRFG_example*.*");
+                ProcessStartInfo cmdsi = new ProcessStartInfo("cmd.exe");
+                cmdsi.WorkingDirectory = HttpContext.Server.MapPath("~/App_Data");
+                cmdsi.Arguments = command;
+                cmdsi.RedirectStandardOutput = true;
+                cmdsi.UseShellExecute = false;
+                cmdsi.CreateNoWindow = false;
+                Process cmd = Process.Start(cmdsi);
+                cmd.WaitForExit();
+
+
+                DateTime t = DateTime.Now;
+                string timeStamp = "_" + t.Year + "_" + t.Month + "_" + t.Day + "_" + t.Hour + "_" + t.Minute + "_" + t.Second + "_" + t.Millisecond;
+                string downloadedFilePath = HttpContext.Server.MapPath("~/App_Data/tas_WRFG_example" + timeStamp + ".nc");
+
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    using (Stream fileStream = System.IO.File.OpenWrite(downloadedFilePath))
+                    {
+                        byte[] buffer = new byte[4096];
+                        int bytesRead = responseStream.Read(buffer, 0, 4096);
+                        while (bytesRead > 0)
+                        {
+                            fileStream.Write(buffer, 0, bytesRead);
+                            DateTime nowTime = DateTime.UtcNow;
+                            if ((nowTime - startTime).TotalMinutes > 5)
+                            {
+                                throw new ApplicationException(
+                                    "Download timed out");
+                            }
+                            bytesRead = responseStream.Read(buffer, 0, 4096);
+                        }
+                    }
+                }
+
+
+                string downloadedFileName = "tas_WRFG_example" + timeStamp + ".nc";
+
+                var dataset = DataSet.Open(downloadedFilePath);
+
+                ViewBag.yc = dataset.GetData<double[]>("yc");
+                ViewBag.xc = dataset.GetData<double[]>("xc");
+                ViewBag.time = dataset.GetData<double[]>("time");
+                var schema = dataset.GetSchema();
+
+                ViewBag.schema = schema;
+
+                ViewBag.level = dataset.GetData<double>("level");
+
+                // tas[,yc=43,xc=67]
+                ViewBag.tas = dataset.GetData<Single[, ,]>("tas");
+
+                string command2 = string.Format("/c dir");
+                ProcessStartInfo cmdsi2 = new ProcessStartInfo("cmd.exe");
+                cmdsi2.WorkingDirectory = HttpContext.Server.MapPath("~/App_Data");
+                cmdsi2.Arguments = command2;
+                cmdsi2.RedirectStandardOutput = true;
+                cmdsi2.UseShellExecute = false;
+                cmdsi2.CreateNoWindow = false;
+                Process cmd2 = Process.Start(cmdsi2);
+                cmd2.WaitForExit();
+
+                commandResponse = cmd2.StandardOutput.ReadToEnd() + commandResponse1;
+
+            }
+            catch (Exception e)
+            {
+                ViewBag.Message = e.Message; ;
+            }
 
             ViewBag.Message = "Test executed correctly:" + commandResponse;
             return View();
