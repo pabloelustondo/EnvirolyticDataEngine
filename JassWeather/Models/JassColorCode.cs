76036b5175fcaeb35e20788c3f09175ea3ce059c
Pablo Elustondo <pablo@jassbrain.com>
2014-03-28 11:53:45 -0400

    Commit Wind Chill works and page shows hours

---
+++ JassWeather/Models/JassColorCode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JassWeather.Models
+{
+    public class JassColorCode
+    {
+        string Color0 { get; set; }
+
+        int Value1 { get; set; }
+        string Color1 { get; set; }
+
+        int Value2 { get; set; }
+        string Color2 { get; set; }
+
+        int Value3 { get; set; }
+        string Color3 { get; set; }
+
+        int Value4 { get; set; }
+        string Color4 { get; set; }
+
+        int Value5 { get; set; }
+        string Color5 { get; set; }
+
+        int Value6 { get; set; }
+        string Color6 { get; set; }
+
+        int Value7 { get; set; }
+        string Color7 { get; set; }
+
+        int Value8 { get; set; }
+        string Color8 { get; set; }
+
+        int Value9 { get; set; }
+        string Color9 { get; set; }
+
+        int Value10 { get; set; }
+        string Color10 { get; set; }
+    }
+}
