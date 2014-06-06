6d1a2a9a83bd89b2db9611be68b5b3f36e7bb3e4
pablo <pelustondo@envirolytic.com>
2014-06-06 15:05:50 -0400

    initial

---
+++ JassWeather/Controllers/ProcessorController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JassWeather.Models;
+
+namespace JassWeather.Controllers
+{
+    public class ProcessorController : Controller
+    {
+        private JassWeatherContext db = new JassWeatherContext();
+
+        //
+        // GET: /Processor/
+
+        public ActionResult Index()
+        {
+            return View(db.JassProcessors.ToList());
+        }
+
+        //
+        // GET: /Processor/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            JassProcessor jassprocessor = db.JassProcessors.Find(id);
+            if (jassprocessor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jassprocessor);
+        }
+
+        //
+        // GET: /Processor/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Processor/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(JassProcessor jassprocessor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.JassProcessors.Add(jassprocessor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(jassprocessor);
+        }
+
+        //
+        // GET: /Processor/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            JassProcessor jassprocessor = db.JassProcessors.Find(id);
+            if (jassprocessor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jassprocessor);
+        }
+
+        //
+        // POST: /Processor/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(JassProcessor jassprocessor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(jassprocessor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(jassprocessor);
+        }
+
+        //
+        // GET: /Processor/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            JassProcessor jassprocessor = db.JassProcessors.Find(id);
+            if (jassprocessor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jassprocessor);
+        }
+
+        //
+        // POST: /Processor/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            JassProcessor jassprocessor = db.JassProcessors.Find(id);
+            db.JassProcessors.Remove(jassprocessor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ JassWeather/Models/JassProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JassWeather.Models
+{
+    public class JassProcessor
+    {
+        public static string statusNew = "new";
+        public static string statusRunning = "running";
+        public static string statusIdleOk = "idle_Ok";
+        public static string statusIdleError = "idle_Error";
+
+        public int JassProcessorID { get; set; }
+        public string name { get; set; }
+        public string status { get; set; }
+        public string info { get; set; }
+        public string update { get; set; }
+        public DateTime? startTime { get; set; }
+        public DateTime? endTime { get; set; }
+        public TimeSpan? spanTime { get; set; }
+        public DateTime lastUpdate { get; set; }
+    }
+}
